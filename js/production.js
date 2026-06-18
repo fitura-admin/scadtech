a6fa276ca684d1b5c7bcc0b4ecfd0a8aa84c2afb
leSchb <kurultae@yandex.ru>
2026-06-18 12:42:20 +0300

    update partners

--- js/production.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.production__text__accordeon__top').onclick = () => {
-    document.querySelector('.production__text__accordeon__top img').classList.toggle('active')
-    let blocks = document.querySelectorAll('.production__text__accordeon__elem')
-    for (let el of blocks) {
-        el.classList.toggle('active')
-    }
-}
